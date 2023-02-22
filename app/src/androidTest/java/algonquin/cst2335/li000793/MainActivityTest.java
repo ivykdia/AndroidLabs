c4cc4f9531503a1a9ebdb16f1c1c5c9a2244b31b
ivykdia <orangieme0604@gmail.com>
2023-02-22 17:44:44 -0500

    week51:done

--- app/src/androidTest/java/algonquin/cst2335/li000793/MainActivityTest.java
+++ app/src/androidTest/java/algonquin/cst2335/li000793/MainActivityTest.java
@@ -1,16 +1,12 @@
 package algonquin.cst2335.li000793;
 
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,24 +32,116 @@ public class MainActivityTest {
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
+    /**
+     * Test case to check if the password requirement in general
+     */
     @Test
     public void mainActivityTest() {
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText)); // tell somebody to go find something in some place.
         appCompatEditText.perform(replaceText("12345"), closeSoftKeyboard());
 
         ViewInteraction materialButton = onView(withId(R.id.button));
         materialButton.perform(click());
 
-
         ViewInteraction textView = onView(withId(R.id.textView));
-
-        textView.check(matches(withText("you shall not pass!")));
+        textView.check(matches(withText("You shall not pass!")));
     }
 
+    /**
+     * Test case to check if at least one upper case is met for the password requirement
+     */
+    @Test
+    public void testFindMissingUpperCase() {
+        // find the view
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText)); // tell somebody to go find something in some place.
+        // type in password123#$*
+        appCompatEditText.perform(replaceText("password123#$*"));
+        // find the button
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        //click the button
+        materialButton.perform(click());
+        //find the text view
+        ViewInteraction textView = onView(withId(R.id.textView));
+        //check the text
+        textView.check(matches(withText("You shall not pass!")));
+    }
 
+    /**
+     * Test case to check if at least one lower case is met for the password requirement
+     */
+    @Test
+    public void testFindMissingLowerCase() {
+        // find the view
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText)); // tell somebody to go find something in some place.
+        // type in PASSWORD123#$*
+        appCompatEditText.perform(replaceText("PASSWORD123#$*"));
+        // find the button
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        //click the button
+        materialButton.perform(click());
+        //find the text view
+        ViewInteraction textView = onView(withId(R.id.textView));
+        //check the text
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * Test case to check if at least one digit number is met for the password requirement
+     */
+    @Test
+    public void testFindMissingDigit() {
+        // find the view
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText)); // tell somebody to go find something in some place.
+        // type in PASSword#$*
+        appCompatEditText.perform(replaceText("PASSword#$*"));
+        // find the button
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        //click the button
+        materialButton.perform(click());
+        //find the text view
+        ViewInteraction textView = onView(withId(R.id.textView));
+        //check the text
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * Test case to check if at least one special character is met for the password requirement
+     */
+    @Test
+    public void testFindMissingSpecialChar() {
+        // find the view
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText)); // tell somebody to go find something in some place.
+        // type in PASSword123
+        appCompatEditText.perform(replaceText("PASSword123"));
+        // find the button
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        //click the button
+        materialButton.perform(click());
+        //find the text view
+        ViewInteraction textView = onView(withId(R.id.textView));
+        //check the text
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * Test case to check if all password requirements are met
+     */
+    @Test
+    public void testMeetRequirement() {
+        // find the view
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText)); // tell somebody to go find something in some place.
+        // type in PASSword123#$*
+        appCompatEditText.perform(replaceText("PASSword123#$*"));
+        // find the button
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        //click the button
+        materialButton.perform(click());
+        //find the text view
+        ViewInteraction textView = onView(withId(R.id.textView));
+        //check the text do an assertion
+        textView.check(matches(withText("Your password meets the requirements")));
+    }
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
